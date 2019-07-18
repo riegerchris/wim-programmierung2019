e9f8c34c6912efdb417f8d85d95141f00cd62ae0
Christoph Rieger <rieger.chris@googlemail.com>
2019-07-18 18:41:58 +0000

    Call by Value Beispiel

---
+++ src/Dog.java
@@ -0,0 +1,14 @@
+
+public class Dog {
+    
+    protected String name;
+    
+    public Dog(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,26 @@
 public class Main {
     
     /**
-     * Einstieg in ein Javaprogramm.
+     * Call-by-Value Beispiel
      */
     public static void main(String[] args){
-        System.out.println("Hello, world!");
+        Dog aDog = new Dog("Max");
+        Dog oldDog = aDog;
+
+       // we pass the object to foo
+        foo(aDog);
+        // aDog variable is still pointing to the "Max" dog when foo(...) returns
+        aDog.getName().equals("Max"); // true
+        aDog.getName().equals("Fifi"); // false
+        System.out.println(aDog == oldDog); // true
+
+    }
+    
+    public static void foo(Dog d) {
+        d.getName().equals("Max"); // true
+        // change d inside of foo() to point to a new Dog instance "Fifi"
+        d = new Dog("Fifi");
+        d.getName().equals("Fifi"); // true
     }
     
 }
